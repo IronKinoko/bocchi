fa57caaf30a4365ec5e8db1ed00a8a18ff8be7bb
IronKinoko <kinoko_main@outlook.com>
2022-11-22 17:27:27 +0800

    fix: resolve sass path

--- packages/bochi/env.d.ts
+++ packages/bochi/env.d.ts
@@ -1,7 +1,49 @@
+/// <reference types="node" />
+
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: 'development' | 'production'
-    APP_NAME: string
-    APP_VERSION: string
+    NODE_ENV: "development" | "production";
+    APP_NAME: string;
+    APP_VERSION: string;
   }
+}
+
+declare module "*.bmp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.scss" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.sass" {
+  const src: string;
+  export default src;
 }
--- tests/bochi/src/index.ts
+++ tests/bochi/src/index.ts
@@ -1,6 +1,3 @@
-import xxxx from "xs";
+import "./index.scss";
 
-const a = 1;
-
-async function test() {}
-console.log(xxxx, a, process.env.NODE_ENV === "development", test);
+console.log();
